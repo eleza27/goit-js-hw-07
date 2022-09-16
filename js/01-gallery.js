fa3801dbc4cb2638f8d0731bf0d427a1a3471bb1
eleza27 <103965241+eleza27@users.noreply.github.com>
2022-09-16 14:11:57 -0400

    HW #7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,9 +22,35 @@ const markup = galleryItems.map(item => `<div class="gallery__item">
 galleryRef.insertAdjacentHTML('afterbegin', markup)
  
 const handler = event => {
-    event.preventDefault()
+  event.preventDefault()
+
+  const imageEl = event.target
+  console.log(imageEl.classList.contains) 
+  if (imageEl.classList.contains('gallery__image')) {
+    const instance = basicLightbox.create(`
+	<img
+      class="gallery__image"
+      src=${imageEl.getAttribute('data-source')}
+      alt=${imageEl.alt}
+    />
+`)
     
+    const modalIsOpen = () => {
+      const handlerKeydown = event => {
+        if (event.key === 'Escape') {
+          instance.close()
+        }
+      }
+      document.addEventListener('keydown', handlerKeydown)
+    }
+    instance.show(modalIsOpen)
+  }
+  
+  console.log(event.target) 
+  console.log(event.currentTarget)    
 }
 
 galleryRef.addEventListener('click', handler)
+
+
 
